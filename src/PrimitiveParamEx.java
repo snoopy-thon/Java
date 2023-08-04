766ff79a5047929c25cd97dd4920b03231cddf97
Yujin <tkfkd7038@naver.com>
2023-08-04 12:38:01 +0900

    예제 6-9 기본형 매개변수일 때 값 변경 안됨

--- src/PrimitiveParamEx.java
+++ src/PrimitiveParamEx.java
@@ -1,8 +1,20 @@
+class Data { int x; }
 
 public class PrimitiveParamEx {
-
 	public static void main(String[] args) {
-
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+		
+		change(d.x);
+		System.out.println("After change(d.x)");
+		System.out.println("main() : x = " + d.x);
+	}
+	
+	// 매개변수가 기본형일 때 메소드에서 변경하려고 해도 실제 값 변경 안됨
+	static void change(int x) {
+		x = 1000;
+		System.out.println("change() : x = " + x);
 	}
 
 }
