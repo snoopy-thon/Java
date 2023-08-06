475d6f00d306973c06c64c0a6749335ec62085a9
Yujin <tkfkd7038@naver.com>
2023-08-06 22:51:03 +0900

    주석 추가

--- src/ch6/ReferenceParamEx3.java
+++ src/ch6/ReferenceParamEx3.java
@@ -4,21 +4,21 @@ public class ReferenceParamEx3 {
 	public static void main(String[] args) {
 		int[] arr = new int[] {3,2,1,6,5,4};
 		
-		printArr(arr);
-		sortArr(arr);
-		printArr(arr);
-		System.out.println("sum="+sumArr(arr));
+		printArr(arr); // 배열의 모든 요소를 출력
+		sortArr(arr);  // 배열을 정렬
+		printArr(arr); // 정렬후 결과를 출력
+		System.out.println("sum="+sumArr(arr)); // 배열의 총합을 출력
 	}
-	static void printArr(int[] arr) {
+	static void printArr(int[] arr) {   // 배열의 모든 요소를 출력
 		System.out.print("[");
 		
-		for(int i : arr)
+		for(int i : arr) // 향상된 for문
 			System.out.print(i+",");
 		
 		System.out.println("]");
 	}
 	
-	static int sumArr(int[] arr) {
+	static int sumArr(int[] arr) {  // 배열의 모든 요소의 합을 반환
 		int sum = 0;
 		
 		for(int i=0; i<arr.length; i++) {
@@ -27,7 +27,7 @@ public class ReferenceParamEx3 {
 		return sum;
 	}
 	
-	static void sortArr(int[] arr) {
+	static void sortArr(int[] arr) {  // 배열을 오름차순으로 정렬
 		for(int i=0; i<arr.length-1;i++) {
 			for(int j=0; j<arr.length-1-i; j++) {
 				if(arr[j] > arr[j+1]) {
