2e741704cf5fc304a0eb47801cf3362326c83b82
Yujin <tkfkd7038@naver.com>
2023-08-11 16:07:17 +0900

    주석 추가

--- src/ch6/MyMath2.java
+++ src/ch6/MyMath2.java
@@ -3,13 +3,13 @@ package ch6;
 class MyMathTest2 {
 	long a, b;
 	
-	// 인스턴스변수 a,b만을 이용해서 작업하므로 매개변수가 필요없다.
+	// 인스턴스 메서드 - 인스턴스변수 a,b만을 이용해서 작업하므로 매개변수가 필요없다.
 	long add() { return a+b;}
 	long subtract() {return a-b;}
 	long multiply() {return a*b;}
 	double divide() {return a/b;}
 	
-	// 인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
+	// 클래스 메서드 - 인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
 	static long add(long a, long b) { return a+b;}
 	static long subtract(long a, long b) { return a-b;}
 	static long multiply(long a, long b) { return a*b;}
