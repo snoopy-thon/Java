1e2455795e8dfa5dc8e74f63e6ce75aded7b3f97
Yujin <tkfkd7038@naver.com>
2023-08-14 22:31:16 +0900

    연습문제 6-6

---
+++ src/practice/Ex6_6.java
@@ -0,0 +1,12 @@
+package practice;
+// 연습문제 6-6
+public class Ex6_6 {
+	// 두 점 (x, y)와 (x1, y1)간의 거리를 구한다.
+	static double getDistance(int x, int y, int x1, int y1) {
+		return Math.sqrt(Math.pow(x-x1,2) + Math.pow(y-y1,2));
+	}
+	public static void main(String[] args) {
+		System.out.println(getDistance(1,1,2,2));
+	}
+
+}
