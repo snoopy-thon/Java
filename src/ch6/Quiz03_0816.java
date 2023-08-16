1bfe4b566e92413db79970be1a0b709e4ac8e435
Yujin <tkfkd7038@naver.com>
2023-08-16 21:51:49 +0900

    0816퀴즈 3번

---
+++ src/ch6/Quiz03_0816.java
@@ -0,0 +1,19 @@
+package ch6;
+// Q3. 5장의 카드를 배열로 받아서 등급을 문자열로 반환하는 rankCheck 메서드를 작성하시오.
+class Card3 {
+	
+	
+	String rankCheck(Card3[] arr) {
+		String rank = "";
+		
+		return rank;
+	}
+}
+
+public class Quiz03_0816 {
+   
+	public static void main(String[] args) {
+		
+	}
+
+}
