61276e3e51c92e7981078da968a0ae915d4ca57a
Yujin <tkfkd7038@naver.com>
2023-08-23 16:31:38 +0900

    예제 8-3 예외처리 try-catch문 추가

---
+++ src/ch8/ExceptionEx3.java
@@ -0,0 +1,17 @@
+package ch8;
+// 예제 8-3 예외처리 try-catch문 추가
+public class ExceptionEx3 {
+    public static void main(String[] args) {
+        int number = 100;
+        int result = 0;
+
+        for(int i=0; i<10; i++){
+            try {
+                result = number / (int) (Math.random() * 10);
+                System.out.println(result);
+            } catch (ArithmeticException e){
+                System.out.println("0");
+            } // try-catch의 끝
+        }// for의 끝
+    }
+}
