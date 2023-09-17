a675cd1b20a71b6563a4e10f956100da447ccd9f
Yujin <tkfkd7038@naver.com>
2023-09-17 21:26:29 +0900

    프로그래머스 문자열을 정수로 변환하기

---
+++ src/programmers/StringToInteger.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class StringToInteger {
+    public int solution(String n_str) {
+        int answer = 0;
+        answer = Integer.parseInt(n_str);
+        return answer;
+    }
+}
