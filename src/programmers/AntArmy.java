7893121dd03cdc39aae70f53adcaa5cedc74fe46
Yujin <tkfkd7038@naver.com>
2023-09-24 23:56:45 +0900

    프로그래머스 개미군단

--- src/programmers/AntArmy.java
+++ src/programmers/AntArmy.java
@@ -2,8 +2,17 @@ package programmers;
 
 public class AntArmy {
     public int solution(int hp) {
-        int answer = 0;
+        int sum = 0;
+        int []antAttackArr = {5,3,1};
+//      1. 장군개미로 나눈다 sum += 23/5, hp = 23%5
+//      2. sum += hp / 3, hp = hp % 3
+//      3. sum += hp / 1
 
-        return answer;
+        for(int i:antAttackArr){
+            sum += hp / i;
+            hp = hp % i;
+        }
+
+        return sum;
     }
 }
