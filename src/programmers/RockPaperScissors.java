a083cf5a1a6a4fd6c1913097b9ecf32b2924e594
Yujin <tkfkd7038@naver.com>
2023-10-03 22:43:10 +0900

    가위 바위 보 게임 구현

--- src/programmers/RockPaperScissors.java
+++ src/programmers/RockPaperScissors.java
@@ -1,4 +1,21 @@
 package programmers;
 
-public class RockPaperScissors {
+
+class RockPaperScissors {
+    public String solution(String rsp) {
+        String answer = "";
+        // 1. 문자열을 하나하나 분리하여 비교한다.
+        // 2. 2 -> 0 / 0 -> 5 / 5 -> 2
+        //    각각 해당하는 값을 result에 넣는다.
+        for(String s : rsp.split("")){
+            if(s.equals("2"))
+                answer += "0";
+            else if(s.equals("0"))
+                answer += "5";
+            else if(s.equals("5"))
+                answer += "2";
+        }
+
+        return answer;
+    }
 }
