51122726f97776cdab242e4d9e243fe85445fca3
Yujin <tkfkd7038@naver.com>
2023-10-10 23:52:59 +0900

    머쓱이네 피자 가게

---
+++ src/programmers/PizzaStore.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class PizzaStore {
+    public int solution(int n) {
+        int answer = 0;
+        // 1. n과 6의 최소 공배수를 구하기
+        // 2. answer = 최소공배수 / 6
+
+        return answer;
+    }
+}
