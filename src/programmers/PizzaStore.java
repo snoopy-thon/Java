9f215ca3380f474329c72e3b264d87fd95d5abe0
Yujin <tkfkd7038@naver.com>
2023-10-11 00:32:23 +0900

    머쓱이네 피자 가게 이중반복문으로 풀기

--- src/programmers/PizzaStore.java
+++ src/programmers/PizzaStore.java
@@ -3,9 +3,23 @@ package programmers;
 public class PizzaStore {
     public int solution(int n) {
         int answer = 0;
-        // 1. n과 6의 최소 공배수를 구하기
-        // 2. answer = 최소공배수 / 6
+        // 1. n과 6의 최소 공배수 구하기
+        //    10*1 10*2 10*3 ...
+        //    6*1 6*2 6*3 ...
 
+        int i = 1, j = 1;
+        while(true){
+            while(n*i >= 6*j){
+                if (n * i == 6 * j) {
+                    answer = j;
+                    break;
+                }
+                j++;
+            }
+            i++;
+            if(answer != 0)
+                break;
+        }
         return answer;
     }
 }
