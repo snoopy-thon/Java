6ff6304a646b4e95a3b7057c516b0d6e894c5527
Yujin <tkfkd7038@naver.com>
2023-11-22 15:51:20 +0900

    두 정수 사이의 합 (테스트코드 통과 못함)

--- src/programmers/SumBetweenTwoInteger.java
+++ src/programmers/SumBetweenTwoInteger.java
@@ -1,10 +1,23 @@
 package programmers;
 
 public class SumBetweenTwoInteger {
-    // 두 정수 사이의 합 
-    public long solution(int a, int b) {
-        long answer = 0;
-
-        return answer;
+    // 두 정수 사이의 합
+    public int solution(int num) {
+        int count = 0;
+        if(num == 1){
+            return 0;
+        }
+        while(num != 1){
+            if(num % 2 == 0){
+                num /= 2;
+            }else{
+                num = (num * 3) + 1;
+            }
+            count ++;
+            if((count == 500) && (num != 1)){
+                return -1;
+            }
+        }
+        return count;
     }
 }
